a898b8d72405cc96501237514a5a8adc43dd0a7e
Jennifer <jnambo@luc.edu>
2024-12-11 23:22:21 -0600

    Initial commit

---
+++ ProblemSolutions.java
@@ -0,0 +1,117 @@
+/******************************************************************
+ *
+ *   Jennifer Nambo / 002
+ *
+ *   This java file contains the problem solutions of isSubSet, findKthLargest,
+ *   and sort2Arrays methods. You should utilize the Java Collection Framework for
+ *   these methods.
+ *
+ ********************************************************************/
+
+import java.util.*;
+
+class ProblemSolutions {
+
+    /**
+     * Method: isSubset()
+     *
+     * Given two arrays of integers, A and B, return whether
+     * array B is a subset if array A. Example:
+     *      Input: [1,50,55,80,90], [55,90]
+     *      Output: true
+     *      Input: [1,50,55,80,90], [55,90, 99]
+     *      Output: false
+     *
+     * The solution time complexity must NOT be worse than O(n).
+     * For the solution, use a Hash Table.
+     *
+     * @param list1 - Input array A
+     * @param list2 - input array B
+     * @return      - returns boolean value B is a subset of A.
+     */
+//checking if given arrays are subsets
+    public boolean isSubset(int list1[], int list2[]) {
+        //will use a hash set to keep and check elements of the arrays (as instructed)
+        Set<Integer> lookingUp = new HashSet<>();
+        //taking the elements from the first array and inserting them into hash set created
+        for (int num : list1) {
+            lookingUp.add(num);
+        }
+        //then we can check if the elements in the second array is in the set we created with the first arrays elements
+        for (int num : list2) {
+            //if at any pt an element is not in the set then false will be returned
+            if (!lookingUp.contains(num)) {
+                return false;
+                }
+            }
+            //if all are in the set then true
+            return true;
+    }
+
+    /**
+     * Method: findKthLargest
+     *
+     * Given an Array A and integer K, return the k-th maximum element in the array.
+     * Example:
+     *      Input: [1,7,3,10,34,5,8], 4
+     *      Output: 7
+     *
+     * @param array - Array of integers
+     * @param k     - the kth maximum element
+     * @return      - the value in the array which is the kth maximum value
+     */
+    //here a pq will be used to keep track of "k-th maximum" element
+    public int findKthLargest(int[] array, int k) {
+        PriorityQueue<Integer> tracking= new PriorityQueue<>();
+            //will go through the given array
+        for (int num : array) {
+            //take element and insert into pq while iterating
+            tracking.offer(num);
+            //if the size is greater than k then remove smallest element
+            if (tracking.size() > k) {
+                //smallest is removed
+                tracking.poll();
+                }
+            }
+            //now the top element is returned since k-th
+            return tracking.peek();
+    }
+
+
+    /**
+     * Method: sort2Arrays
+     *
+     * Given two arrays A and B with n and m integers respectively, return
+     * a single array of all the elements in A and B in sorted order. Example:
+     *      Input: [4,1,5], [3,2]
+     *      Output: 1 2 3 4 5
+     *
+     * @param array1    - Input array 1
+     * @param array2    - Input array 2
+     * @return          - Sorted array with all elements in A and B.
+     */
+
+    public int[] sort2Arrays(int[] array1, int[] array2) {
+        //pq will be used again to merge and sort arrays
+        PriorityQueue<Integer> mergeSort = new PriorityQueue<>();
+        //take elements from the first array and put them into the pq
+        for (int num : array1) {
+            //inserting
+            mergeSort.offer(num);
+        }
+        //then the same will be done for the second array
+        for (int num : array2) {
+            //inserting here
+            mergeSort.offer(num);
+        }
+        //now we can make an array with both arrays combined
+        int[] finalArr = new int[array1.length + array2.length];
+        //using poll will help make sure as they are being moved from the pq to
+        //the final array that they are kept in order
+        for (int i = 0; i < finalArr.length; i++) {
+                finalArr[i] = mergeSort.poll();
+            }
+        //final array is returned
+            return finalArr;
+        }
+}
